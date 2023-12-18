c64dfc5f9932415804d3973c2c73946df99456cf
Dyani <dyani70@gmail.com>
2023-12-18 18:49:06 +0900

    Initial Commit

---
+++ FastSwiftBasic/FastSwiftBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastSwiftBasicApp.swift
+//  FastSwiftBasic
+//
+//  Created by Dyani on 2023/12/18.
+//
+
+import SwiftUI
+
+@main
+struct FastSwiftBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
