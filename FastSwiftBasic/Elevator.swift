2fefe87b1419c8ba29d84e060c74ae5e2c853ab5
Dyani <dyani70@gmail.com>
2024-01-01 22:43:26 +0900

    Struct

---
+++ FastSwiftBasic/Elevator.swift
@@ -0,0 +1,18 @@
+//
+//  Elevator.swift
+//  FastSwiftBasic
+//
+//  Created by Dyani on 1/1/24.
+//
+
+import SwiftUI
+
+struct Elevator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Elevator()
+}
