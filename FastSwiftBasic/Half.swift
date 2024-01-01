ba8372089762880dc93ddc2710bb7f264e208c0a
Dyani <dyani70@gmail.com>
2024-01-01 22:11:21 +0900

    mid-term review

---
+++ FastSwiftBasic/Half.swift
@@ -0,0 +1,18 @@
+//
+//  Half.swift
+//  FastSwiftBasic
+//
+//  Created by Dyani on 1/1/24.
+//
+
+import SwiftUI
+
+struct Half: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Half()
+}
