9afe0db9509e5c6cc2216986ecdf6c4758771bcd
Dyani <dyani70@gmail.com>
2024-01-02 22:19:38 +0900

    ObserableObject

---
+++ FastSwiftBasic/Diff.swift
@@ -0,0 +1,18 @@
+//
+//  Diff.swift
+//  FastSwiftBasic
+//
+//  Created by Dyani on 1/1/24.
+//
+
+import SwiftUI
+
+struct Diff: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Diff()
+}
