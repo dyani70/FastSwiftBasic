d0ec574e5abaa537435b0bd299ec6c3539a0be11
Dyani <dyani70@gmail.com>
2024-01-02 23:50:23 +0900

    enum and switch

---
+++ FastSwiftBasic/Choice.swift
@@ -0,0 +1,18 @@
+//
+//  Choice.swift
+//  FastSwiftBasic
+//
+//  Created by Dyani on 1/2/24.
+//
+
+import SwiftUI
+
+struct Choice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Choice()
+}
