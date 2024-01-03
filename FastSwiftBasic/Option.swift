15ced589a0e78738b041b3150b25716c8956c6b3
Dyani <dyani70@gmail.com>
2024-01-03 23:35:22 +0900

    Optional Binding, Optional Chaining

---
+++ FastSwiftBasic/Option.swift
@@ -0,0 +1,18 @@
+//
+//  Option.swift
+//  FastSwiftBasic
+//
+//  Created by Dyani on 1/3/24.
+//
+
+import SwiftUI
+
+struct Option: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Option()
+}
