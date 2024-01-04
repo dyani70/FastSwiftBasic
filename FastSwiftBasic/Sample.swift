9ecb703ae1cf66a780f5c9219c9da278a3af9cde
Dyani <dyani70@gmail.com>
2024-01-04 23:36:07 +0900

    final review

---
+++ FastSwiftBasic/Sample.swift
@@ -0,0 +1,18 @@
+//
+//  Sample.swift
+//  FastSwiftBasic
+//
+//  Created by Dyani on 1/4/24.
+//
+
+import SwiftUI
+
+struct Sample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sample()
+}
