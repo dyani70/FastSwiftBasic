06159de7e8cccc9e3354a97f723c088d91270aea
Dyani <dyani70@gmail.com>
2024-01-05 23:56:09 +0900

    Exception

---
+++ FastSwiftBasic/Exception.swift
@@ -0,0 +1,18 @@
+//
+//  Exception.swift
+//  FastSwiftBasic
+//
+//  Created by Dyani on 1/5/24.
+//
+
+import SwiftUI
+
+struct Exception: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Exception()
+}
