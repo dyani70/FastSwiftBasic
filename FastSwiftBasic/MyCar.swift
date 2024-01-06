39322c87ce53a58f346f2071e50129e2f783f9bf
Dyani <dyani70@gmail.com>
2024-01-06 23:27:36 +0900

    Protocol

---
+++ FastSwiftBasic/MyCar.swift
@@ -0,0 +1,18 @@
+//
+//  MyCar.swift
+//  FastSwiftBasic
+//
+//  Created by Dyani on 1/6/24.
+//
+
+import SwiftUI
+
+struct MyCar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyCar()
+}
