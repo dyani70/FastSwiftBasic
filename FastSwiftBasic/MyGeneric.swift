ffaf6fa79bf84ae2c538c713c0798ad29a234b20
Dyani <dyani70@gmail.com>
2024-01-07 22:34:05 +0900

    Generic

---
+++ FastSwiftBasic/MyGeneric.swift
@@ -0,0 +1,18 @@
+//
+//  MyGeneric.swift
+//  FastSwiftBasic
+//
+//  Created by Dyani on 1/7/24.
+//
+
+import SwiftUI
+
+struct MyGeneric: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyGeneric()
+}
